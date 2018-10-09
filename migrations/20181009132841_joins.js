a54da0f1b84bd70f7489584014f6475e93c0ef8e
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-09 14:55:45 -0600

    joins migration exports.up edited

--- migrations/20181009132841_joins.js
+++ migrations/20181009132841_joins.js
@@ -5,8 +5,8 @@ exports.up = function(knex, Promise) {
         join.integer('authorId')
         join.integer('bookId')
 
-        join.foreign('authorId').references('id').inTable('authors')
-        join.foreign('bookId').references('id').inTable('books');
+        join.foreign('authorId').references('id').inTable('authors_table')
+        join.foreign('bookId').references('id').inTable('books_table');
     });
 };
 
