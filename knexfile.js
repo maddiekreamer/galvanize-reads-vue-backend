53b2e77c821bc8ff7a3fb30765fc4fd6cc293986
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-09 13:11:16 -0600

    books migrations created

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgresql://localhost/books_info'
+    connection: 'postgresql://localhost/galvanize_reads'
   },
 
   production: {
