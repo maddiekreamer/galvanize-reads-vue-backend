111a21731fec9a96d990ec3493c6db444182d05f
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-09 13:11:44 -0600

    changes saved

--- migrations/20181009113849_books.js
+++ migrations/20181009113849_books.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('books_table', (book) => {
+        book.increments('id')
+        book.string('title')
+        book.string('genre')
+        book.string('description')
+        book.string('coverURL')
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('books')
 };
