b7ce8945908d20ecdbda982729a08d219d127b2d
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-09 10:35:02 -0600

    Basic get route added and tested through Postman

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/", (request, response) => {
+    response.send('THE ROUTE WORKED!');
+})
+
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
+    console.log(`listening on ${port}`);
 })
