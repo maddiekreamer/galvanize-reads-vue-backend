46d7b16f24ea401646443867b459d562afc6296f
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-10 11:31:09 -0600

    queries updated, get routes added, added cors requirement

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const queries = require('./queries');
+const cors = require("cors")
 
-app.get("/", (request, response) => {
-    response.send('THE ROUTE WORKED!');
-})
+app.use(cors())
+
+app.get("/", (request, response, next) => {
+    queries.getBooks().then(result => response.json({
+        result
+    }))
+});
+
+app.get("/", (request, response, next) => {
+    queries.getAuthors().then(result => response.json({
+        result
+    }))
+});
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
--- queries.js
+++ queries.js
@@ -3,7 +3,7 @@ const database = require('knex')(connection)
 
 getBooks = () => database.select().from('books_table').orderBy('id', 'desc')
 
-getAuthors = (id) => database.select().from('books_table').where('firstName', id)
+getAuthors = (id) => database.select().from('authors_table').orderBy('id', 'desc')
 
 module.exports = {
     getBooks,
