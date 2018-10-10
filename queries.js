fc1513f37c122ee067bd7cfb81659076ff7574d5
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-10 11:21:57 -0600

    getBooks and getAuthors queries added

--- queries.js
+++ queries.js
@@ -1,6 +1,11 @@
 const connection = require('./knexfile')[process.env.NODE_ENV || 'development']
 const database = require('knex')(connection)
 
+getBooks = () => database.select().from('books_table').orderBy('id', 'desc')
+
+getAuthors = (id) => database.select().from('books_table').where('firstName', id)
+
 module.exports = {
-    
+    getBooks,
+    getAuthors
 }
